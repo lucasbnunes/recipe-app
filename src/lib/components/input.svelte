fce89899327c39b3f4a74a1a6650486a256d96fe
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-06 14:46:54 -0300

    feat: replace outline with ring to allow css transitions

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -12,7 +12,7 @@
 
 <div
 	class={[
-		'flex items-center gap-2 rounded-lg bg-concrete-100 px-4 py-2 focus-within:outline-2 focus-within:outline-concrete-500',
+		'flex items-center gap-2 rounded-lg bg-concrete-100 px-4 py-2 focus-within:ring-2',
 		className
 	]}
 >
@@ -24,7 +24,7 @@
 
 	<input
 		{...props}
-		class="form-input h-full w-full flex-1 rounded-lg border-none bg-transparent p-0 px-1 text-base leading-normal font-normal outline-0 placeholder:text-concrete-500"
+		class="form-input h-full w-full flex-1 rounded-lg border-none bg-transparent p-0 px-1 text-base leading-normal font-normal !ring-0 placeholder:text-concrete-500"
 	/>
 	{#if endAdornment}
 		<div class="text-concrete-500">
