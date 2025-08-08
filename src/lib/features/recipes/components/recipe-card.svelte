052df7e2c7f12a5a932bd167c70a0f27accf5ade
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-08 15:35:54 -0300

    feat: add recipe card and recipes grid components

---
+++ src/lib/features/recipes/components/recipe-card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import type { Recipe } from '../types';
+
+	type RecipeCardProps = SvelteHTMLElements['div'] & {
+		recipe: Recipe;
+	};
+
+	const { recipe, class: className, ...props }: RecipeCardProps = $props();
+</script>
+
+<div class={['flex w-full min-w-[180px] flex-col gap-2 sm:max-w-[240px]', className]} {...props}>
+	<div class="h-[135px] overflow-hidden rounded-lg">
+		<img alt={`${recipe.name} picture`} src={recipe.image} class="w-full" />
+	</div>
+
+	<div class="flex flex-col">
+		<p class="font-medium">{recipe.name}</p>
+		<div class="flex gap-2 text-concrete-600">
+			<span>
+				{recipe.difficulty}
+			</span>
+			<span> • </span>
+			<span class="flex items-center">{recipe.cookTimeMinutes + recipe.prepTimeMinutes}min</span>
+		</div>
+	</div>
+</div>
---
+++ src/lib/features/recipes/components/recipes-grid.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import type { Recipe } from '../types';
+	import RecipeCard from './recipe-card.svelte';
+
+	type RecipesGridProps = SvelteHTMLElements['ul'] & {
+		recipes: Recipe[];
+	};
+
+	const { recipes, class: className, ...props }: RecipesGridProps = $props();
+</script>
+
+<ul
+	class={[
+		'grid max-w-full grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-x-4 gap-y-6 sm:justify-start',
+		className
+	]}
+	{...props}
+>
+	{#each recipes as recipe}
+		<li>
+			<RecipeCard {recipe} />
+		</li>
+	{/each}
+</ul>
