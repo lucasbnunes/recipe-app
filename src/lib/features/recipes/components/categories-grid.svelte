171ac9ffe2acf5759473ca78de03ab4d893c93f3
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-11 15:27:22 -0300

    feat: add categories grid

---
+++ src/lib/features/recipes/components/categories-grid.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import snack from '$lib/assets/snacks.png';
+	import breakfast from '$lib/assets/breakfast.png';
+	import lunch from '$lib/assets/lunch.png';
+	import dinner from '$lib/assets/dinner.png';
+	import dessert from '$lib/assets/dessert.png';
+	import CategoryCard from './category-card.svelte';
+
+	const CATEGORIES = [
+		{
+			title: 'Snacks',
+			url: '/snacks',
+			image: snack
+		},
+		{
+			title: 'Dessert',
+			url: '/dessert',
+			image: dessert
+		},
+		{
+			title: 'Lunch',
+			url: '/lunch',
+			image: lunch
+		},
+		{
+			title: 'Dinner',
+			url: '/dinner',
+			image: dinner
+		},
+		{
+			title: 'Breakfast',
+			url: '/breakfast',
+			image: breakfast
+		}
+	];
+</script>
+
+<ul
+	class="grid max-w-full grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] gap-x-4 gap-y-6 sm:justify-start"
+>
+	{#each CATEGORIES as category}
+		<CategoryCard {category} />
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import CategoriesGrid from '$lib/features/recipes/components/categories-grid.svelte';
 	import RecipesGrid from '$lib/features/recipes/components/recipes-grid.svelte';
 	import type { PageProps } from './$types';
 
@@ -7,3 +8,6 @@
 
 <h2 class="mb-7">Popular recipes</h2>
 <RecipesGrid recipes={data.recipes} />
+
+<h2 class="mt-9 mb-7">Explore by meal type</h2>
+<CategoriesGrid />
