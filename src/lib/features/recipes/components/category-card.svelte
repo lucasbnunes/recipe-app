b2e6388218b3846103d0ea566505c5b606c470da
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-11 14:28:03 -0300

    feat: create category card component

---
+++ src/lib/features/recipes/components/category-card.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	type Category = {
+		title: string;
+		url: string;
+		image: string;
+	};
+
+	type CategoryCardProps = SvelteHTMLElements['a'] & {
+		category: Category;
+	};
+
+	const { class: className, category, ...props }: CategoryCardProps = $props();
+</script>
+
+<a
+	href={`/recipes/${category.url.replace('/', '')}`}
+	class={['flex w-full min-w-[180px] flex-col gap-2 sm:max-w-[180px]', className]}
+	{...props}
+>
+	<div class="overflow-hidden rounded-lg">
+		<img alt={`${category.title} picture`} src={category.image} class="w-full" />
+	</div>
+
+	<div class="flex flex-col">
+		<p class="font-medium">{category.title}</p>
+	</div>
+</a>
