712a74d03a6e6d428a1a3b1bc89ea81e4904d6cb
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-12 13:03:58 -0300

    feat: add request to fetch recipes

--- src/lib/features/recipes/api/recipesApi.ts
+++ src/lib/features/recipes/api/recipesApi.ts
@@ -3,11 +3,18 @@ import type { RecipeListResponse } from '../types';
 
 export const recipesApi = {
 	fetchPopularRecipes: () => {
+		/* the API doesn't provide a "popular recipes" endpoint,
+		 so we fetch 4 results for demonstration purpose */
 		return httpClient.get<RecipeListResponse>('', {
 			searchParams: new URLSearchParams({
 				limit: '4',
 				skip: '20'
 			})
+		});
+	},
+	fetchRecipes: (params?: URLSearchParams) => {
+		return httpClient.get<RecipeListResponse>('', {
+			searchParams: params
 		});
 	}
 };
