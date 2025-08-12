1b993d708182db5182767551a2f06e625bd14fff
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-12 13:08:12 -0300

    feat: implement grid on the recipes page

--- src/lib/features/recipes/components/recipes-grid.svelte
+++ src/lib/features/recipes/components/recipes-grid.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	import type { Recipe } from '../types';
+	import type { SvelteHTMLElements } from 'svelte/elements';
 	import RecipeCard from './recipe-card.svelte';
 
-	interface RecipesGridProps {
+	type RecipesGridProps = SvelteHTMLElements['ul'] & {
 		recipes: Recipe[];
-	}
+	};
 
-	const { recipes }: RecipesGridProps = $props();
+	const { recipes, class: className, ...props }: RecipesGridProps = $props();
 </script>
 
 <ul
-	class="grid max-w-full grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] gap-x-4 gap-y-6 sm:justify-start"
+	class={[
+		'grid max-w-full grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] gap-x-4 gap-y-6 sm:justify-start',
+		className
+	]}
+	{...props}
 >
 	{#each recipes as recipe}
 		<RecipeCard {recipe} />
---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import RecipesGrid from '$lib/features/recipes/components/recipes-grid.svelte';
+	import type { PageProps } from './$types';
+
+	const { data }: PageProps = $props();
+</script>
+
+<h1>Recipes</h1>
+<p class="mt-2 max-w-3xl text-concrete-700">
+	Explore a world of flavors with our curated collection of recipes. Find inspiration for every
+	meal, from quick weeknight dinners to elaborate weekend feasts.
+</p>
+
+<RecipesGrid recipes={data.recipes} class="mt-7" />
