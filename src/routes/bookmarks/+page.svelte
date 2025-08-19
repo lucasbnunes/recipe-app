a550fa44b59a0e0acd7163457fef96eb5df9e637
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-19 11:44:08 -0300

    feat: add optimistic updates to the bookmarks list

--- src/routes/bookmarks/+page.svelte
+++ src/routes/bookmarks/+page.svelte
@@ -5,11 +5,16 @@
 	import { bookmarks } from '$lib/features/recipes/state/bookmarks-state.svelte';
 
 	let recipesPromise = $state(recipesApi.fetchRecipesByIds(bookmarks.get().map(String)));
+	let bookmarkedIds = $derived(new Set(bookmarks.get()));
+
+	$effect(() => {
+		bookmarkedIds = new Set(bookmarks.get());
+	});
 </script>
 
 <h1 class="mb-3">Bookmarks</h1>
 {#await recipesPromise}
 	<div class="flex h-[30vh] items-center justify-center"><Loader /></div>
 {:then recipes}
-	<RecipesGrid {recipes} />
+	<RecipesGrid recipes={recipes.filter((item) => bookmarkedIds.has(item.id))} />
 {/await}
