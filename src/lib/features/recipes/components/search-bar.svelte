014708c3b128f4992f5b5546896e66cc9ea26bd2
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-19 15:15:38 -0300

    fix: prevent error when trying to access wndows object

--- src/lib/features/recipes/components/search-bar.svelte
+++ src/lib/features/recipes/components/search-bar.svelte
@@ -3,23 +3,27 @@
 	import type { InputProps } from '$lib/components/input.svelte';
 	import Input from '$lib/components/input.svelte';
 	import { Search } from '@lucide/svelte';
+	import { onMount } from 'svelte';
 
 	const {
 		name = 'q',
-		endAdornment,
+		endAdornment: endAdornmentProp,
 		...props
 	}: InputProps & {
 		name?: string;
 	} = $props();
 
-	const searchParams = new URLSearchParams(window.location.search);
-	let searchQuery = $state(searchParams.get(name));
+	let searchQuery = $state('');
+	onMount(() => {
+		const searchParams = new URLSearchParams(window.location.search);
+		searchQuery = searchParams.get(name) || '';
+	});
 </script>
 
 <Input placeholder="Search recipes" class="my-8" {name} bind:value={searchQuery} {...props}>
 	{#snippet endAdornment()}
-		{#if endAdornment}
-			{@render endAdornment()}
+		{#if endAdornmentProp}
+			{@render endAdornmentProp()}
 		{:else}
 			<IconButton type="submit">
 				<Search />
