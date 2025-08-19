ef69319701d5742d7d5ab0a95189c2601d659ea5
Lucas Brito <lucasbnunes23@gmail.com>
2025-08-19 14:48:53 -0300

    feat: integrate SearchBar component into the main page for recipe search functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import CategoriesGrid from '$lib/features/recipes/components/categories-grid.svelte';
 	import PopularRecipesGrid from '$lib/features/recipes/components/popular-recipes-grid.svelte';
+	import SearchBar from '$lib/features/recipes/components/search-bar.svelte';
 	import type { PageProps } from './$types';
 
 	const { data }: PageProps = $props();
 </script>
+
+<form method="GET" action={`/recipes`}>
+	<SearchBar />
+</form>
 
 <h2 class="mb-7">Popular recipes</h2>
 <PopularRecipesGrid recipes={data.recipes} />
